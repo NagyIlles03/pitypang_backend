952b49c3fb81634f0d168d9a28a3d8cf02238dfa
zsidoruben <zsido.ruben@students.jedlik.eu>
2023-02-01 12:53:24 +0100

    export,import

---
+++ data/export_bookings_from_atlas.bat
@@ -0,0 +1,1 @@
+"c:\Program Files\MongoDB\Server\5.0\bin\mongoexport.exe" --uri="mongodb+srv://user1:user1@pitypang.ki4zcjn.mongodb.net/Pitypang" --collection=people --out=bookings_new.json --jsonArray
