63293613e43f8050c0f0b27f0cbfbd3c73e8041b
NagyIlles03 <nagy.illes@students.jedlik.eu>
2023-03-22 10:30:30 +0100

    Cleaned report

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import App from "./app";
 import AuthenticationController from "./authentication/authentication.controller";
-import ReportController from "./report/report.controller";
 import UserController from "./user/user.controller";
 
-const app = new App([new AuthenticationController(), new UserController(), new ReportController()]);
+const app = new App([new AuthenticationController(), new UserController()]);
 
 app.listen();
